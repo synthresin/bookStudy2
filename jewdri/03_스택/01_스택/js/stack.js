3b1f9c2651a99f7cf10e2a87511b214bc3fd3082
DESKTOP-BJ41PHT\Jewdri <ksoldier86@naver.com>
2017-03-27 20:32:55 +0900

    지난주꺼(3장스택) 다시 커밋

---
+++ jewdri/03_스택/01_스택/js/stack.js
@@ -0,0 +1,33 @@
+﻿function stack(){
+	this.item = [];
+}
+
+//뒤에 원소 추가
+stack.prototype.push = function(n){
+	this.item.push(n);
+}
+//뒤에꺼 삭제
+stack.prototype.pop = function(){
+	return this.item.pop();
+}
+//꼭대기 반환(맨뒤)
+stack.prototype.peek = function(){
+	return this.item[this.item.length-1];
+}
+//isEmpty
+stack.prototype.isEmpty = function(){
+	return this.item.length == 0;
+}
+//사이즈
+stack.prototype.size = function(){
+	return this.item.length;
+}
+//클리어
+stack.prototype.clear = function(){
+	this.item = [];
+}
+//프린트 toString() : 배열을 문자열로 변환하여 반환하는 배열메소드
+stack.prototype.print = function(){
+	console.log(this.item.toString());
+	return this.item.toString();
+}
