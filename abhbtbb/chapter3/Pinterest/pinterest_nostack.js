7e83938e6e28bf7c7837c7f2f07b8ffa47784a79
Chulhun Park <abhbtbb1@gmail.com>
2017-03-30 22:35:46 +0900

    핀터레스트 작업중

---
+++ abhbtbb/chapter3/Pinterest/pinterest_nostack.js
@@ -0,0 +1,10 @@
+imageFiles = [br_conllection.jpg,br_shop_01.jpg,br_shop_02.jpg,br_shop_03.jpg,img_case_type_02.jpg,img_case_type_04.jpg,img_filter_01.jpg,img_filter_07.jpg];
+
+var itemTemplate = '<li class="card">\
+                        <img class="image">\
+                        <dt>\
+                            <dd class="title"></dd>\
+                            <dd class="content"></dd>\
+                            <dd class="user_id"></dd>\
+                        </dt>\
+                    </li>'
